9215b1d45f95350bd3ec4d842f02f8e97ea96919
faiq69280 <faiq69280@gmail.com>
2024-08-12 13:32:02 -0700

    initial

---
+++ src/main/java/com/creativeuncommons/ProgressTrackingSystem/ProgressTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.creativeuncommons.ProgressTrackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creativeuncommons/ProgressTrackingSystem/ProgressTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creativeuncommons.ProgressTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
